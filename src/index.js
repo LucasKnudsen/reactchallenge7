5ac2bd3586dd083e475c1d40b3fceda9d9cb52d6
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-07 16:10:54 +0100

    Fetching data from API, storing in state variable, and displaying

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+  const [books, setBooks] = useState([])
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const fetchData = async () => {
+    let response = await fetch(apiURL).then(response => response.json())
+
+    setBooks(response)
+  }
+
+  const bookList = books.map((book, i) => {
+    return (
+      <div className="book" key={i}>
+        <h3>Book Number {i + 1}</h3>
+        <h2>{book.name}</h2>
+
+        <div className="details">
+          <p>👨: Author/Authors: {book.authors }</p>
+          <p>📖: Number of pages: {book.numberOfPages }</p>
+          <p>🏘️: Book Country: {book.country }</p>
+          <p>⏰: Release date: {new Date(book.released).toLocaleDateString('sv') }</p>
+        </div>
+      </div>
+    )
+  })
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={() => fetchData()}>
+          Fetch Data
+          </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+
+      {/* Use JSX below for each book */}
+      <div className="books">
+        {bookList}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
